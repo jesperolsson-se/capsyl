d2d60f62ea76beb2bcc08a9cd485ba6b79ac6e18
Jesper Olsson <jesper@jesperolsson.se>
2020-11-22 18:42:48 +0100

    #3 Redefine AST analysis for better mental models

--- src/main/java/se/jesperolsson/capsyl/Main.java
+++ src/main/java/se/jesperolsson/capsyl/Main.java
@@ -1,17 +1,24 @@
 package se.jesperolsson.capsyl;
 
+import se.jesperolsson.capsyl.encapsulation.Encapsulation;
+import se.jesperolsson.capsyl.encapsulation.Encapsulations;
+
 import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        new VoidVisitorStarter(
+        Iterable<Encapsulation> encapsulations = new Encapsulations(
                 new File(
                         args.length >= 1
                                 ? args[0]
                                 : "src/main/java/se/jesperolsson/capsyl/App.txt"
                 )
-        ).print();
+        ).asIterable();
+
+        for(Encapsulation encapsulation : encapsulations) {
+            System.out.println(encapsulation.represent());
+        }
     }
 }
