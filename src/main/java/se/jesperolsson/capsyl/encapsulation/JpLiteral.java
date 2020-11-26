6c1a14f939d6dcbdae13ee95737aaeb47bcf00ba
Jesper Olsson <jesper@jesperolsson.se>
2020-11-26 07:42:40 +0100

    #4 Document JpLiteral

--- src/main/java/se/jesperolsson/capsyl/encapsulation/JpLiteral.java
+++ src/main/java/se/jesperolsson/capsyl/encapsulation/JpLiteral.java
@@ -1,23 +1,32 @@
+/*
+ * Capsyl is licenced under GPL-3.0. More info is found in ${basedir}/LICENCE.
+ */
 package se.jesperolsson.capsyl.encapsulation;
 
 import com.github.javaparser.ast.expr.LiteralExpr;
 
 /**
  * Encapsulatee adapter for a JavaParser literal expression.
+ *
+ * @since 0.1
  */
-public class JpLiteral implements Encapsulatee {
+public final class JpLiteral implements Encapsulatee {
 
+    /**
+     * JavaParser representation of literal expressions.
+     */
     private final LiteralExpr literal;
 
-    public JpLiteral(LiteralExpr literal) {
+    /**
+     * Constructs a literal representation from a JavaParser concept.
+     * @param literal A JavaParser representation of a literal.
+     */
+    public JpLiteral(final LiteralExpr literal) {
         this.literal = literal;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String name() {
-        return literal.toString();
+        return this.literal.toString();
     }
 }
