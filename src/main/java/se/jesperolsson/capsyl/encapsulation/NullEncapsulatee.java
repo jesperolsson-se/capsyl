389b8d9aa7084e3dd330222a2c9b126002b13fd6
Jesper Olsson <jesper@jesperolsson.se>
2020-11-26 18:33:41 +0100

    #4 Document NullEncapsulatee

--- src/main/java/se/jesperolsson/capsyl/encapsulation/NullEncapsulatee.java
+++ src/main/java/se/jesperolsson/capsyl/encapsulation/NullEncapsulatee.java
@@ -1,20 +1,27 @@
+/*
+ * Capsyl is licenced under GPL-3.0. More info is found in ${basedir}/LICENCE.
+ */
 package se.jesperolsson.capsyl.encapsulation;
 
 /**
  * Null object for an encapsulatee.
+ *
+ * @since 0.1
  */
 public final class NullEncapsulatee implements Encapsulatee {
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String name() {
         return "";
     }
 
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(final Object obj) {
         return obj instanceof NullEncapsulatee;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
     }
 }
