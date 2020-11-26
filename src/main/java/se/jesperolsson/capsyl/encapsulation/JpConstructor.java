c49dbb55a8381219f3c425ec1339bf6dcf75b079
Jesper Olsson <jesper@jesperolsson.se>
2020-11-26 07:38:23 +0100

    #4 Document JpConstructor

--- src/main/java/se/jesperolsson/capsyl/encapsulation/JpConstructor.java
+++ src/main/java/se/jesperolsson/capsyl/encapsulation/JpConstructor.java
@@ -1,23 +1,32 @@
+/*
+ * Capsyl is licenced under GPL-3.0. More info is found in ${basedir}/LICENCE.
+ */
 package se.jesperolsson.capsyl.encapsulation;
 
 import com.github.javaparser.ast.expr.ObjectCreationExpr;
 
 /**
  * Encapsulatee adapter for a JavaParser constructor expression.
+ *
+ * @since 0.1
  */
-public class JpConstructor implements Encapsulatee {
+public final class JpConstructor implements Encapsulatee {
 
+    /**
+     * JavaParser representation of a constructor expression.
+     */
     private final ObjectCreationExpr constructor;
 
-    public JpConstructor(ObjectCreationExpr constructor) {
+    /**
+     * Constructs a constructor representation from a JavaParser concept.
+     * @param constructor A JavaParser representation of a constructor.
+     */
+    public JpConstructor(final ObjectCreationExpr constructor) {
         this.constructor = constructor;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String name() {
-        return constructor.getTypeAsString();
+        return this.constructor.getTypeAsString();
     }
 }
